76900e5d597d4bff29047977f55a1a8aade17fcc
Chaicoo <gabriellimma27@gmail.com>
2024-05-19 11:54:25 -0300

    fix: adjusts type in user id

--- src/users/dto/response-user.dto.ts
+++ src/users/dto/response-user.dto.ts
@@ -1,12 +1,13 @@
 import { UserEntity } from '../entities/user.entity';
 
 export class ResponseUserDto {
-  id: string;
+  id: number;
   firstName: string;
   lastName: string;
   isActive: boolean;
 
   constructor(user: Partial<UserEntity>) {
+    this.id = user.id;
     this.firstName = user.firstName;
     this.lastName = user.lastName;
     this.isActive = user.isActive;
